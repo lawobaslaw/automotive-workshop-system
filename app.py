c8ceb2d331477942918f5aedabfb8de22f9edf08
Larry Obabueki <lawobaslaw@gmail.com>
2026-07-08 09:28:17 +0100

    Day 2: Added vehicles page

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/vehicles")
+def vehicles():
+    cars = [
+        "Mercedes EQC",
+        "BMW 320d",
+        "Ford Focus",
+        "Toyota Corolla"
+    ]
+
+    return render_template("vehicles.html", cars=cars)
+
 if __name__ == "__main__":
     app.run(debug=True)
