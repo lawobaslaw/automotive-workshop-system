fbbdae5fe45f218da9dc4c41337a39dc1dbf69ff
Larry Obabueki <lawobaslaw@gmail.com>
2026-07-09 10:55:20 +0100

    Vehicle register table

--- app.py
+++ app.py
@@ -9,10 +9,30 @@ def home():
 @app.route("/vehicles")
 def vehicles():
     cars = [
-        "Mercedes EQC",
-        "BMW 320d",
-        "Ford Focus",
-        "Toyota Corolla"
+        {
+        "registration": "LP21 EQC",
+        "make": "Mercedes",
+        "model": "EQC",
+        "year": 2021
+    },
+    {
+        "registration": "BM19 320",
+        "make": "BMW",
+        "model": "320d",
+        "year": 2019
+    },
+    {
+        "registration": "FD20 FCS",
+        "make": "Ford",
+        "model": "Focus",
+        "year": 2020
+    },
+    {
+        "registration": "TY22 COR",
+        "make": "Toyota",
+        "model": "Corolla",
+        "year": 2022
+    }
     ]
 
     return render_template("vehicles.html", cars=cars)
